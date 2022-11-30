a1368b766529c4dd9700025286367aca886d5a6a
chu1111 <113088657+chu1111@users.noreply.github.com>
2022-11-30 11:08:49 +0800

    Add files via upload

---
+++ Class15.java
@@ -0,0 +1,8 @@
+import static java.lang.System.out;
+public class Class15 {
+    public static void main(String[] args) {
+        String str = String.valueOf(123.45);
+        out.println(str);
+    
+    }
+}
